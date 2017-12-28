7ab035890ed30ed38d99475132c0ed368057552f
Lars <lars.mueller@libracore.com>
2017-12-28 14:09:03 +0100

    script folder and parse codeblock

--- myapp/myapp/page/bankimport/bankimport.py
+++ myapp/myapp/page/bankimport/bankimport.py
@@ -4,9 +4,20 @@ import frappe
 from email_reply_parser import EmailReplyParser
 from markdown2 import markdown
 
+def parse(content):
+    # this parses the content
+    lines = content.split("\n")
+    for line in lines:
+        fields = line.split("\t")
+        for field in fields:
+            print("Field: " + field)
+            
+    return
+
 @frappe.whitelist()
 def parse_file(content):
 	# content is the plain text content, parse 
 	frappe.throw("Got a file!" + content)
 
 	return
+
