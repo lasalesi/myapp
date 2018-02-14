cc8a07672bc6c6b3bcb3896fcadd6b15ef512c3a
lasalesi <larsericmueller@gmail.com>
2018-02-14 14:50:12 +0100

    configurable report based on view

--- myapp/myapp/doctype/sales_report/sales_report.js
+++ myapp/myapp/doctype/sales_report/sales_report.js
@@ -2,16 +2,33 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Sales Report', {
-	refresh: function(frm) {
-
-	},
     setup: function(frm) {
-        frappe.call({
-            method: 'fill',
-            doc: frm.doc,
-            callback: function(response) {
-                refresh_field(['items', 'week', 'date', 'this_year', 'last_year']);
-            }
-        }); 
-    }
+        if (frm.doc.name.startsWith("New")) {
+            frappe.call({
+                method: 'fill',
+                doc: frm.doc,
+                callback: function(response) {
+                    refresh_field(['items', 'week', 'date', 'this_year', 'last_year']);
+                }
+            }); 
+        }
+    },
+    refresh: function(frm) {
+        // add utility buttons
+        frm.add_custom_button(__("Update totals"), function() {
+            frappe.call({
+                method: 'update_totals',
+                doc: frm.doc,
+                callback: function(response) {
+                    refresh_field(['total_qty_7days', 'total_qty_7days',
+                    'total_qty_ytd', 'total_demand_qty_ytd',
+                    'total_demand_qty_py', 'total_qty_py',
+                    'total_revenue_7days', 'total_revenue_ytd',
+                    'total_demand_revenue_ytd', 'total_demand_revenue_py',
+                    'total_revenue_py']);
+                }
+            });
+        });
+	}
+
 });
--- myapp/myapp/doctype/sales_report/sales_report.py
+++ myapp/myapp/doctype/sales_report/sales_report.py
@@ -9,13 +9,13 @@ from datetime import datetime
 
 class SalesReport(Document):       
     def onload(self):
-        pass
+        return
 
     def setup(self):
-        pass
+        return
               
     def refresh(self):
-        pass
+        return
         
     """ This function is called in a script fashion to populate the report 
         It can be executed from the wrapper
@@ -33,49 +33,58 @@ class SalesReport(Document):
         self.last_year = _last_year
         
         # define each line item
-        _description = "Octocat"
-        _sql_7days = """SELECT (IFNULL(SUM(`qty`), 0)) AS `qty`, 
-                (IFNULL(SUM(`net_amount`), 0)) AS `revenue`
-            FROM `tabDelivery Note Item`
-            WHERE `item_code` LIKE 'Octocat'
-            AND `docstatus` = 1
-            AND `creation` > DATE_SUB(NOW(), INTERVAL 7 DAY)
-            """
-        _qty_7days,_revenue_7days = get_qty_revenue(_sql_7days)
-        _sql_YTD = """SELECT (IFNULL(SUM(`qty`), 0)) AS `qty`, 
-                (IFNULL(SUM(`net_amount`), 0)) AS `revenue`
-            FROM `tabDelivery Note Item`
-            WHERE `item_code` LIKE 'Octocat'
-            AND `docstatus` = 1
-            AND `creation` >= '{0}-01-01'
-            """.format(today.strftime("%Y"))
-        _qty_YTD,_revenue_YTD = get_qty_revenue(_sql_YTD)
-        _sql_PY = """SELECT (IFNULL(SUM(`qty`), 0)) AS `qty`, 
-                (IFNULL(SUM(`net_amount`), 0)) AS `revenue`
-            FROM `tabDelivery Note Item`
-            WHERE `item_code` LIKE 'Octocat'
-            AND `docstatus` = 1
-            AND `creation` >= '{0}-01-01'
-            AND `creation` <= '{0}-12-31'
-            """.format(int(today.strftime("%Y")) - 1)
-        _qty_PY,_revenue_PY = get_qty_revenue(_sql_PY)
-                        
-        self.append('items', 
-            { 
-                'description': _description, 
-                'qty_7days': _qty_7days,
-                'revenue_7days': _revenue_7days,
-                'qty_ytd': _qty_YTD,
-                'revenue_ytd': _revenue_YTD, 
-                'qty_py': _qty_PY,
-                'revenue_py': _revenue_PY,
-                'demand_qty_ytd': (_qty_YTD/_week),
-                'demand_revenue_ytd': (_revenue_YTD/_week),
-                'demand_qty_py': (_qty_PY/52), 
-                'demand_revenue_py': (_revenue_PY/52)
-            })
+        line_items = [{'field': 'item_code', 'value': 'Octocat'},
+            {'field': 'item_group', 'value': 'Products'},
+            {'field': 'item_group', 'value': 'Services'}]
+        
+        for line_item in line_items:
+            _description = line_item['value']
+            _sql_7days = """SELECT (IFNULL(SUM(`qty`), 0)) AS `qty`, 
+                    (IFNULL(SUM(`net_amount`), 0)) AS `revenue`
+                FROM `viewDelivery`
+                WHERE `{0}` LIKE '{1}'
+                AND `docstatus` = 1
+                AND `posting_date` > DATE_SUB(NOW(), INTERVAL 7 DAY)
+                """.format(line_item['field'], line_item['value'])
+            _qty_7days,_revenue_7days = get_qty_revenue(_sql_7days)
+            _sql_YTD = """SELECT (IFNULL(SUM(`qty`), 0)) AS `qty`, 
+                    (IFNULL(SUM(`net_amount`), 0)) AS `revenue`
+                FROM `viewDelivery`
+                WHERE `{1}` LIKE '{2}'
+                AND `docstatus` = 1
+                AND `posting_date` >= '{0}-01-01'
+                """.format(today.strftime("%Y"), line_item['field'], line_item['value'])
+            _qty_YTD,_revenue_YTD = get_qty_revenue(_sql_YTD)
+            _sql_PY = """SELECT (IFNULL(SUM(`qty`), 0)) AS `qty`, 
+                    (IFNULL(SUM(`net_amount`), 0)) AS `revenue`
+                FROM `viewDelivery`
+                WHERE `{1}` LIKE '{2}'
+                AND `docstatus` = 1
+                AND `posting_date` >= '{0}-01-01'
+                AND `posting_date` <= '{0}-12-31'
+                """.format(int(today.strftime("%Y")) - 1, line_item['field'], line_item['value'])
+            _qty_PY,_revenue_PY = get_qty_revenue(_sql_PY)
+                            
+            self.append('items', 
+                { 
+                    'description': _description, 
+                    'qty_7days': _qty_7days,
+                    'revenue_7days': _revenue_7days,
+                    'qty_ytd': _qty_YTD,
+                    'revenue_ytd': _revenue_YTD, 
+                    'qty_py': _qty_PY,
+                    'revenue_py': _revenue_PY,
+                    'demand_qty_ytd': (_qty_YTD/_week),
+                    'demand_revenue_ytd': (_revenue_YTD/_week),
+                    'demand_qty_py': (_qty_PY/52), 
+                    'demand_revenue_py': (_revenue_PY/52)
+                })
         
         # compute totals
+        self.update_totals()
+        return
+    
+    def update_totals(self):
         _total_qty_7days = 0
         _total_qty_ytd = 0 
         _total_demand_qty_ytd = 0
@@ -86,18 +95,21 @@ class SalesReport(Document):
         _total_demand_revenue_ytd = 0
         _total_demand_revenue_py = 0
         _total_revenue_py = 0
-        for item in doc.items:
-            _total_qty_7days = _total_qty_7days + item.qty_7days
-            _total_qty_ytd = _total_qty_ytd + item.qty_ytd
-            _total_demand_qty_ytd = _total_demand_qty_ytd + item.demand_qty_ytd
-            _total_demand_qty_py = _total_demand_qty_py + item.demand_qty_py
-            _total_qty_py = _total_qty_py + item.qty_py
-            _total_revenue_7days = _total_revenue_7days + item.revenue_7days
-            _total_revenue_ytd = _total_revenue_ytd + item.revenue_ytd
-            _total_demand_revenue_ytd = _total_demand_revenue_ytd + item.demand_revenue_ytd
-            _total_demand_revenue_py = _total_demand_revenue_py + item.demand_revenue_py
-            _total_revenue_py = _total_revenue_py + item.revenue_py
-        
+        try:
+            if self.items:
+                for item in self.items:
+                    _total_qty_7days = _total_qty_7days + item.qty_7days
+                    _total_qty_ytd = _total_qty_ytd + item.qty_ytd
+                    _total_demand_qty_ytd = _total_demand_qty_ytd + item.demand_qty_ytd
+                    _total_demand_qty_py = _total_demand_qty_py + item.demand_qty_py
+                    _total_qty_py = _total_qty_py + item.qty_py
+                    _total_revenue_7days = _total_revenue_7days + item.revenue_7days
+                    _total_revenue_ytd = _total_revenue_ytd + item.revenue_ytd
+                    _total_demand_revenue_ytd = _total_demand_revenue_ytd + item.demand_revenue_ytd
+                    _total_demand_revenue_py = _total_demand_revenue_py + item.demand_revenue_py
+                    _total_revenue_py = _total_revenue_py + item.revenue_py
+        except:
+            pass
         self.total_qty_7days = _total_qty_7days
         self.total_qty_ytd = _total_qty_ytd 
         self.total_demand_qty_ytd = _total_demand_qty_ytd
@@ -107,7 +119,8 @@ class SalesReport(Document):
         self.total_revenue_ytd = _total_revenue_ytd
         self.total_demand_revenue_ytd = _total_demand_revenue_ytd
         self.total_demand_revenue_py = _total_demand_revenue_py
-        self.total_revenue_py = _total_revenue_py
+        self.total_revenue_py = _total_revenue_py     
+        return   
         
 @frappe.whitelist()
 def create_report():
