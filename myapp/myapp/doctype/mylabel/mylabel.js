3c8e6ee5d3428f0690d177475f9376669694c3f4
lasalesi <larsericmueller@gmail.com>
2018-07-12 23:14:25 +0200

    mylabel improvements

--- myapp/myapp/doctype/mylabel/mylabel.js
+++ myapp/myapp/doctype/mylabel/mylabel.js
@@ -2,22 +2,23 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('MyLabel', {
-	refresh: function(frm) {
-		frm.add_custom_button(__("PDF!"), function() {
-			create_pdf(frm);
-		}).addClass("btn-primary");
-	}
+    refresh: function(frm) {
+	frm.add_custom_button(__("PDF!"), function() {
+	    create_pdf(frm);
+	}).addClass("btn-primary");
+    }
 	
 });
 
 function create_pdf(frm) {
-	var w = window.open(
-		frappe.urllib.get_full_url("/api/method/myapp.myapp.doctype.mylabel.mylabel.download_mylabel?"  
-				+ "name=" + encodeURIComponent(me.frm.doc.name))
-	);
-	if (!w) {
-		frappe.msgprint(__("Please enable pop-ups")); return;
-	}
+    var w = window.open(
+	    frappe.urllib.get_full_url("/api/method/myapp.myapp.doctype.mylabel.mylabel.download_mylabel?"  
+			    + "name=" + encodeURIComponent(me.frm.doc.name))
+    );
+    if (!w) {
+	    frappe.msgprint(__("Please enable pop-ups")); return;
+    }
+    
 }
 
 function download(filename, content) {
